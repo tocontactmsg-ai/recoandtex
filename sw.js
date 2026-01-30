fe1250117b40f8cf8c3a81759749ea9646ca2a67
tocontactmsg-ai <tocontactmsg@gmail.com>
2026-01-30 15:53:12 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('fahem-cache').then(cache => {
+      return cache.addAll(['./']);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
